2fe9ab6dee447fe3e87542ce4eb30edd94d115e7
Yongzhi Xu <785931268@qq.com>
2021-02-16 21:53:58 +0800

    first app cmd

--- kubernetes-course/first-app/cmd/create-first-app.sh
+++ kubernetes-course/first-app/cmd/create-first-app.sh
@@ -2,6 +2,7 @@ echo "create first app"
 
 kubectl get node
 kubectl get pod
+kubectl get pods -o wide
 
 echo "set up docker repository secrete"
     kubectl create secret docker-registry myregistrysecret \
@@ -16,5 +17,14 @@ kubectl describe pod helloworldnodejs.example.com
 
 kubectl port-forward helloworldnodejs.example.com 8081:3001
 echo "or create the service"
-kubectl expose pod helloworldnodejs.example.com
+kubectl expose pod helloworldnodejs.example.com --type=NodePort --name helloworldnodejs-service
 kubectl get service
+kubectl describe services service-name
+
+echo "attach to a pod"
+kubectl attach helloworldnodejs.example.com
+echo "exec cmd inside the pod"
+kubectl exec helloworldnodejs.example.com -- ls /app 
+
+
+
