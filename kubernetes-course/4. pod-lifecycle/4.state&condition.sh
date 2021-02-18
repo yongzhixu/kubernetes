2f7d2a875b6b5fa3fc83be05f30ee203f867d78b
xuyongzhi <785931268@qq.com>
2021-02-18 23:14:29 +0800

    pod life cycle

---
+++ kubernetes-course/4. pod-lifecycle/4.state&condition.sh
@@ -0,0 +1,14 @@
+echo "get pod state"
+kubectl get pods # pending/runing/failure/succeeded
+
+echo "get pod condition "
+kubectl describe pod podname # ready/initialized/podscheduled
+
+echo "get containers states"
+kubectl get pod podname -n namesapce -o yaml # running/terminating/waiting 
+
+
+
+
+
+
